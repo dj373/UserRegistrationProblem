f6f5b883a1ce17168806029114c4c84c569d6642
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-15 17:41:33 +0530

    Added: Check for email format

--- UserValidation.sh
+++ UserValidation.sh
@@ -9,8 +9,18 @@ function checkName() {
 		echo "Invalid format! Name should be atleast 3 letters long and start with Capital letter."
 	fi
 }
-
+function checkEmail() {
+	emailCheck="^[0-9a-zA-Z]+([.+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+	if [[ $1 =~ $emailCheck ]]
+	then
+		echo "Valid email!"
+	else
+		echo "Invalid format!"
+	fi
+}
 read -p "Enter first name (John) " fname
 checkName $fname
 read -p "Enter last name (Doe) " lname
 checkName $lname
+read -p "Enter email address (johndoe@jd.com) " email
+checkEmail $email
